8ebba50e4f9737b580aad67ce6069e2abfa96e6f
if1live <libsora25@gmail.com>
2017-05-17 10:48:01 +0900

    unity version test code

---
+++ unity/Assets/Editor/BuildHelper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildHelper : MonoBehaviour {
+    [MenuItem("Game/idenfitier_com.fiveminlab.toyclash")]
+    static void SetAs_com_fiveminlab_toyclash() {
+        var id = "com.fiveminlab.toyclash";
+        SetIdentifier(id);
+    }
+
+    [MenuItem("Game/idenfitier_com.fiveminlab.Toyclash")]
+    static void SetAs_com_fiveminlab_Toyclash() {
+        var id = "com.fiveminlab.Toyclash";
+        SetIdentifier(id);
+    }
+
+    static void SetIdentifier(string id) {
+        PlayerSettings.applicationIdentifier = id;
+        Debug.LogFormat("identifier : {0}", id);
+    }
+}
---
+++ unity/Assets/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+    public Text text;
+
+    void Start() {
+        Refresh();
+    }
+
+    private void Update() {
+        if(Input.GetButtonDown("Fire1")) {
+            Refresh();
+        }
+    }
+
+    void Refresh() {
+        var sb = new StringBuilder();
+
+        sb.Append(Application.identifier);
+        sb.AppendLine();
+
+        var size = Screen.currentResolution;
+        sb.AppendFormat("width: {0}", size.width);
+        sb.AppendLine();
+        sb.AppendFormat("Height: {0}", size.height);
+
+        text.text = sb.ToString();
+    }
+}
